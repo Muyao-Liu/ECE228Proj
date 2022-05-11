7334b50bd660de35168631220d10a64305b41e45
MonicaHui <monica.zihui@gmail.com>
2022-05-11 15:28:46 -0700

    add edge smooth

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ from torch.autograd import Variable
 from torchvision import datasets
 
 import matplotlib.pyplot as plt
-import network
+import network, util
 
 parser = argparse.ArgumentParser()
 
@@ -23,18 +23,19 @@ parser.add_argument('--init_num_epoch', type=int, default=10,
                     help='num of initialization epoch')
 parser.add_argument('--batch_size', type=int, default=8, help='batch size')
 parser.add_argument('--root_path', default=1, help='batch size')
-parser.add_argument('--lr_G', type=float, default=0.0002,
+parser.add_argument('--lr_G', type=float, default=0.0005,
                     help='learning rate of Generator')
-parser.add_argument('--lr_D', type=float, default=0.00102,
+parser.add_argument('--lr_D', type=float, default=0.0005,
                     help='learning rate of Discriminator')
 parser.add_argument('--gamma_G', type=float, default=0.1, help='gamma_G')
 parser.add_argument('--gamma_D', type=float, default=0.1, help='gamma_D')
 parser.add_argument('--beta_1', type=float, default=0.5, help='beta_1')
 parser.add_argument('--beta_2', type=float, default=0.99, help='beta_2')
-parser.add_argument('--cont_lambda', type=float, default=1.5, help='cont_lambda')
+parser.add_argument('--cont_lambda', type=float, default=10, help='cont_lambda')
 parser.add_argument('--gray_lambda', type=float, default=3, help='gray_lambda')
 parser.add_argument('--load_model', type=bool, default=False, help='load previous model')
-parser.add_argument('--is_pretrained', type=bool, default=True, help='is pretrained')
+parser.add_argument('--is_pretrained', type=bool, default=False, help='is pretrained')
+parser.add_argument('--is_smoothed', type=bool, default=True, help='is smoothed')
 
 opt = parser.parse_args()
 
@@ -61,6 +62,7 @@ D.to(device)
 vgg = vgg19(pretrained=False)
 vgg.load_state_dict(torch.load('./vgg19.pth'))
 # conv4_4
+# print(vgg)
 vgg = vgg.features[:26]
 vgg.to(device)
 vgg.eval()
@@ -76,13 +78,15 @@ src_path = "./data/real/train"
 cart_path = "./data/cartoon/train"
 cart_smooth_path = "./data/cartoon/edge_smoothed"
 
-
-    
+if not opt.is_smoothed:
+    if not os.path.isdir(cart_smooth_path):
+        os.mkdir(cart_smooth_path)
+    util.edge_smooth(cart_path, cart_smooth_path)
 
 
 src_loader = torch.utils.data.DataLoader(datasets.ImageFolder(src_path, transform), batch_size=opt.batch_size, shuffle=True, drop_last=True)
 cartoon_loader = torch.utils.data.DataLoader(datasets.ImageFolder(cart_path, transform), batch_size=opt.batch_size, shuffle=True, drop_last=True)
-# cartoon_smooth_loader = torch.utils.data.DataLoader(datasets.ImageFolder(cart_smooth_path, transform), batch_size=opt.batch_size, shuffle=True, drop_last=True)
+cartoon_smooth_loader = torch.utils.data.DataLoader(datasets.ImageFolder(cart_smooth_path, transform), batch_size=opt.batch_size, shuffle=True, drop_last=True)
 
 
 
@@ -95,11 +99,9 @@ def pretrain():
 
     for epoch in range(opt.init_num_epoch):
         Cont_losses = []
-        start_time = time.time()
+        
 
         for i, img in enumerate(zip(src_loader, cartoon_loader)):
-            if i > 0:
-                break
             src, cart = img[0][0], img[1][0]
             src = src.to(device)
             cart = cart.to(device)
@@ -121,9 +123,11 @@ def pretrain():
             train_hist['Cont_losses'].append(Cont_loss.item())
 
             if i % 50 == 0:
-                print("i: " , i , "Content_loss: " ,Cont_loss.item())
-                result = torch.cat((src[0], gen_cart[0]), 2)
-                result = (result.cpu().detach().numpy().transpose(1, 2, 0) + 1) / 2
+                print("i: %s, Content_loss: %.3f" % (i, Cont_loss.item()))
+                real = src[0].cpu().detach().numpy().transpose(1, 2, 0)
+                cart = gen_cart[0].cpu().detach().numpy().transpose(1, 2, 0)
+                result = np.concatenate((real, cart), axis=1)
+                result = (result + 1) / 2
                 filename = "during_pretrain_%s_%s.png" % (epoch, i)
                 path = os.path.join("./result", filename)
                 plt.imsave(path, result)
@@ -165,10 +169,12 @@ def train():
 
     for epoch in range(opt.num_epoch):
         
+        start_time = time.time()
+        
         G_losses = []
         D_losses = []
         Cont_losses = []
-        for i, img in enumerate(zip(src_loader, cartoon_loader, cartoon_loader)):
+        for i, img in enumerate(zip(src_loader, cartoon_loader, cartoon_smooth_loader)):
             
             src, cart, cart_smooth = img[0][0], img[1][0], img[2][0]
             src = src.to(device)
