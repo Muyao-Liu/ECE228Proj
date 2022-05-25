0b4557eca2dafb2d90c98063d8f4da6ef6d42c20
MonicaHui <monica.zihui@gmail.com>
2022-05-25 13:28:13 -0700

    modify network

--- network.py
+++ network.py
@@ -34,8 +34,70 @@ class ResidualBlock(nn.Module):
 
     def forward(self, x):
         return x + self.residual_conv(x)
-
+    
 class generator(nn.Module):
+    def __init__(self, dim_in = 3, dim_out = 3, hidden_dim = 64):
+        super(generator, self).__init__()
+        self.in_conv = nn.Sequential(
+            nn.Conv2d(dim_in, hidden_dim, kernel_size = 7, stride = 1, padding = 3),
+            nn.InstanceNorm2d(hidden_dim),
+            nn.ReLU(inplace=True)
+        )
+
+        self.down_conv = nn.Sequential(
+            nn.Conv2d(hidden_dim, hidden_dim * 2, kernel_size = 3, stride = 2, padding = 1),
+            nn.Conv2d(hidden_dim * 2, hidden_dim * 2, kernel_size = 3, stride = 1, padding = 1),
+            nn.InstanceNorm2d(hidden_dim * 2),
+            nn.ReLU(inplace=True),
+
+            nn.Conv2d(hidden_dim * 2, hidden_dim * 4, kernel_size = 3, stride = 2, padding = 1),
+            nn.Conv2d(hidden_dim * 4, hidden_dim * 4, kernel_size = 3, stride = 1, padding = 1),
+            nn.InstanceNorm2d(hidden_dim * 4),
+            nn.ReLU(inplace=True)
+        )
+
+        # residual_blocks
+        self.residual_blocks = nn.Sequential(
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4),
+            ResidualBlock(dim_in=hidden_dim * 4, dim_out=hidden_dim * 4)
+        )
+
+
+
+        self.up_conv = nn.Sequential(
+            nn.ConvTranspose2d(hidden_dim * 4, hidden_dim * 2, kernel_size = 3, stride = 2, padding = 1, output_padding = 1),
+            nn.Conv2d(hidden_dim * 2, hidden_dim * 2, kernel_size = 3, stride = 1, padding = 1),
+            nn.InstanceNorm2d(hidden_dim * 2),
+            nn.ReLU(inplace=True),
+
+            nn.ConvTranspose2d(hidden_dim * 2, hidden_dim, kernel_size = 3, stride = 2, padding = 1, output_padding = 1),
+            nn.Conv2d(hidden_dim, hidden_dim, kernel_size = 3, stride = 1, padding = 1),
+            nn.InstanceNorm2d(hidden_dim),
+            nn.ReLU(inplace=True)
+        )
+        
+        self.out_conv = nn.Sequential(
+            nn.Conv2d(hidden_dim, dim_out, kernel_size = 7, stride = 1, padding = 3),
+        )
+
+    def forward(self, x):
+        in_down = self.in_conv(x)
+        out_down = self.down_conv(in_down)
+        out_res = self.residual_blocks(out_down)
+        out_up = self.up_conv(out_res)
+        output = self.out_conv(out_up)
+        return output
+    
+    
+    
+
+class generator_att(nn.Module):
     def __init__(self, dim_in = 3, dim_out = 3, hidden_dim = 64):
         super(generator, self).__init__()
         self.in_conv = nn.Sequential(
